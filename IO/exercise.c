7a9d12a38f022ade583092ba97c438793d145288
Nathnael Getachew <nathangetachew536@gmail.com>
2026-09-13 15:24:41 +0300

    Add exercise.c with formatted output examples

---
+++ IO/exercise.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void ex_4() {
+    printf("%#012.5g\n", 83.7361);
+    printf("%#012.5g\n", 29748.6607);
+    printf("%#012.5g\n", 1054932234.0);
+    printf("%#012.5g\n", 0.0000235218);
+}
+
+
+int main(void) {
+    ex_4();
+
+    return 0;
+}
